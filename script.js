eed8707b5eaafc8a8da2565bbed9efb4007022dc
Manish Jaunjalkar <117558314+manishjaunjalkar@users.noreply.github.com>
2023-04-12 15:46:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -24,12 +24,14 @@ class Teacher extends Person {
 	}
 }
 
-const person = new Person(John, 25);
+const person = new Person("JOhn", 25);
 console.log(person.name);
 Person.age = 30;
 const student = new Student("Jane",20);
+console.log(student.name);
 study();
 const teacher = new Teacher("Bob", 35);
+console.log(teacher.name);
 teach();
 
 // Do not change the code below this line
